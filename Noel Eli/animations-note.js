1f93c0df3cd9ccbcd3a2d0f0b866b6617c6f278f
ValGuerin-Lanarre <110185176+ValGuerin-Lanarre@users.noreply.github.com>
2024-12-21 21:07:30 +0100

    Delete Noel Eli directory

--- Noel Eli/animations-note.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const noteCard = document.querySelector('.note-card');
-    const heart = document.querySelector('.heart');
-
-    // Ajout d'animation dynamique si nécessaire (exemple d'événements futurs)
-    setTimeout(() => {
-        heart.style.animation = "draw-heart 2s ease-out forwards";
-    }, 2000);
-});
